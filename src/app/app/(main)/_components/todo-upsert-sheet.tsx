01d857f513df06c29a8fe9438d3eca141696ace8
RobertooFelipe <feliperoberto092@gmail.com>
2024-03-15 20:56:51 -0300

    feat: generate a basic home page of tasks

---
+++ src/app/app/(main)/_components/todo-upsert-sheet.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import React, { useRef } from "react";
+import { Todo } from "./todo-data-table";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+
+type TodoUpsertSheetProps = {
+  children?: React.ReactNode;
+  defaultValue?: Todo;
+};
+
+export function TodoUpsertSheet({
+  children,
+  defaultValue,
+}: TodoUpsertSheetProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const form = useForm();
+
+  const onSubmit = form.handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <div ref={ref}>{children}</div>
+      </SheetTrigger>
+      <SheetContent>
+        <Form {...form}>
+          <form
+            onSubmit={onSubmit}
+            className="space-y-8 h-screen flex flex-col"
+          >
+            <SheetHeader>
+              <SheetTitle>Create Todo</SheetTitle>
+              <SheetDescription>
+                Make changes to your profile here. Click save when you &lsquo;
+                re done.
+              </SheetDescription>
+            </SheetHeader>
+
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your todo title" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public todo title.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <SheetFooter>
+              <Button type="submit">Save changes</Button>
+            </SheetFooter>
+          </form>
+        </Form>
+      </SheetContent>
+    </Sheet>
+  );
+}
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,17 +1,34 @@
 import {
   DashboardPage,
   DashboardPageHeader,
+  DashboardPageHeaderNav,
   DashboardPageHeaderTitle,
   DashboardPageMain,
 } from "@/components/dashboard/page";
+import { TodoDataTable } from "./(main)/_components/todo-data-table";
+import { TodoUpsertSheet } from "./(main)/_components/todo-upsert-sheet";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "@radix-ui/react-icons";
 
 export default async function AppPage() {
   return (
     <DashboardPage>
       <DashboardPageHeader>
         <DashboardPageHeaderTitle>Tarefas</DashboardPageHeaderTitle>
+        <DashboardPageHeaderNav>
+          <DashboardPageHeaderNav>
+            <TodoUpsertSheet>
+              <Button variant="outline" size="sm">
+                <PlusIcon className="w-4 h-4 mr-3" />
+                Add todo
+              </Button>
+            </TodoUpsertSheet>
+          </DashboardPageHeaderNav>
+        </DashboardPageHeaderNav>
       </DashboardPageHeader>
-      <DashboardPageMain>Tarefas</DashboardPageMain>
+      <DashboardPageMain>
+        <TodoDataTable />
+      </DashboardPageMain>
     </DashboardPage>
   );
 }
